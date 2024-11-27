dfa19af1b08a97f31682afeac1c1c425b81bc0f9
Andrii Lytvyn <109283742+AndriiLyvyn@users.noreply.github.com>
2024-11-27 22:17:27 +0100

    Update dictioneryScript.js

--- scripts/dictioneryScript.js
+++ scripts/dictioneryScript.js
@@ -28,46 +28,46 @@ const container = document.getElementById("container");
       switch (element) {
         case (element = "Dictionary A-Z"):
           window.location.href =
-            "/pages/dictionaryPages/dictionaryAlphabet.html";
+            "https://andriilyvyn.github.io/language-page/pages/dictionaryPages/dictionaryAlphabet.html";
           break;
         case (element = "Level A1"):
           window.location.href =
-            "/pages/dictionaryPages/dictionaryLevel-A1.html";
+            "https://andriilyvyn.github.io/language-page/pages/dictionaryPages/dictionaryLevel-A1.html";
           localStorage.setItem("level", "A1");
           break;
         case (element = "Level A2"):
           window.location.href =
-            "/pages/dictionaryPages/dictionaryLevel-A2.html";
+            "https://andriilyvyn.github.io/language-page/pages/dictionaryPages/dictionaryLevel-A2.html";
           localStorage.setItem("level", "A2");
           break;
         case (element = "Level B1"):
           window.location.href =
-            "/pages/dictionaryPages/dictionaryLevel-B1.html";
+            "https://andriilyvyn.github.io/language-page/pages/dictionaryPages/dictionaryLevel-B1.html";
           localStorage.setItem("level", "B1");
           break;
         case (element = "Level B2"):
           window.location.href =
-            "/pages/dictionaryPages/dictionaryLevel-B2.html";
+            "https://andriilyvyn.github.io/language-page/pages/dictionaryPages/dictionaryLevel-B2.html";
           localStorage.setItem("level", "B2");
           break;
         case (element = "Level C1"):
           window.location.href =
-            "/pages/dictionaryPages/dictionaryLevel-C1.html";
+            "https://andriilyvyn.github.io/language-page/pages/dictionaryPages/dictionaryLevel-C1.html";
           localStorage.setItem("level", "C1");
           break;
         case (element = "Level C2"):
           window.location.href =
-            "/pages/dictionaryPages/dictionaryLevel-C2.html";
+            "https://andriilyvyn.github.io/language-page/pages/dictionaryPages/dictionaryLevel-C2.html";
           localStorage.setItem("level", "C2");
           break;
         case (element = "Irregular verbs"):
-          window.location.href = "/pages/dictionaryPages/irregular_verb.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/dictionaryPages/irregular_verb.html";
           break;
         case (element = "Clothes"):
-          window.location.href = "/pages/dictionaryPages/clothes.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/dictionaryPages/clothes.html";
           localStorage.setItem("level", "Clothes");
         case (element = "Kitchen"):
-          window.location.href = "/pages/dictionaryPages/kitchen.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/dictionaryPages/kitchen.html";
           localStorage.setItem("level", "Kitchen");
       }
     });
