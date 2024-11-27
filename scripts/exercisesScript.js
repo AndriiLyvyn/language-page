619276e1a20b93dda95c6018dc82813d239acaee
Andrii Lytvyn <109283742+AndriiLyvyn@users.noreply.github.com>
2024-11-27 22:26:12 +0100

    Update exercisesScript.js

--- scripts/exercisesScript.js
+++ scripts/exercisesScript.js
@@ -76,11 +76,11 @@ function verbBlock(data) {
     listItems.addEventListener("click", () => {
       switch (element) {
         case (element = "Past tense"):
-          window.location.href = "/pages/verbs.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/verbs.html";
           localStorage.setItem("getKeyLevel", "Past");
           break;
         case (element = "Past participle"):
-          window.location.href = "/pages/verbs.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/verbs.html";
           localStorage.setItem("getKeyLevel", "Participle");
           break;
       }
@@ -97,35 +97,35 @@ function vocabularyBlock(data) {
     listItems.addEventListener("click", () => {
       switch (element) {
         case (element = "Level A1"):
-          window.location.href = "/pages/vocabulary.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/vocabulary.html";
           localStorage.setItem("getKeyLevel", "A1");
           break;
         case (element = "Level A2"):
-          window.location.href = "/pages/vocabulary.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/vocabulary.html";
           localStorage.setItem("getKeyLevel", "A2");
           break;
         case (element = "Level B1"):
-          window.location.href = "/pages/vocabulary.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/vocabulary.html";
           localStorage.setItem("getKeyLevel", "B1");
           break;
         case (element = "Level B2"):
-          window.location.href = "/pages/vocabulary.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/vocabulary.html";
           localStorage.setItem("getKeyLevel", "B2");
           break;
         case (element = "Level C1"):
-          window.location.href = "/pages/vocabulary.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/vocabulary.html";
           localStorage.setItem("getKeyLevel", "C1");
           break;
         case (element = "Level C2"):
-          window.location.href = "/pages/vocabulary.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/vocabulary.html";
           localStorage.setItem("getKeyLevel", "C2");
           break;
         case (element = "Clothes"):
-          window.location.href = "/pages/vocabulary.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/vocabulary.html";
           localStorage.setItem("getKeyLevel", "Clothes");
           break;
         case (element = "Kitchen"):
-          window.location.href = "/pages/vocabulary.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/vocabulary.html";
           localStorage.setItem("getKeyLevel", "Kitchen");
           break;
       }
@@ -150,84 +150,84 @@ function readBlock(data, block) {
 
       switch (element) {
         case (element = "A Day at the Zoo"):
-          window.location.href = "/pages/readText/A1/a_day_at_the_zoo.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/A1/a_day_at_the_zoo.html";
           break;
         case (element = "A Trip to the Beach"):
-          window.location.href = "/pages/readText/A1/a_trip_to_the_beach.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/A1/a_trip_to_the_beach.html";
           break;
         case (element = "My Daily Routine"):
-          window.location.href = "/pages/readText/A1/my_daily_routine.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/A1/my_daily_routine.html";
           break;
         case (element = "My Family"):
-          window.location.href = "/pages/readText/A1/my_Family.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/A1/my_Family.html";
           break;
         case (element = "My Favorite Hobby"):
-          window.location.href = "/pages/readText/A1/my_favorite_hobby.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/A1/my_favorite_hobby.html";
           break;
         case (element = "My Pet Cat"):
-          window.location.href = "/pages/readText/A1/my_pet_cat.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/A1/my_pet_cat.html";
           break;
         case (element = "My Pet"):
-          window.location.href = "/pages/readText/A1/my_Pet.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/A1/my_Pet.html";
           break;
         case (element = "My School"):
-          window.location.href = "/pages/readText/A1/my_school.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/A1/my_school.html";
           break;
         case (element = "A Day in the City"):
-          window.location.href = "/pages/readText/A2/a_day_in_the_city.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/A2/a_day_in_the_city.html";
           break;
         case (element = "My Best Friend"):
-          window.location.href = "/pages/readText/A2/my_best_friend.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/A2/my_best_friend.html";
           break;
         case (element = "A Visit to the Library"):
-          window.location.href = "/pages/readText/A2/visit_to_the_library.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/A2/visit_to_the_library.html";
           break;
         case (element = "A Weekend Trip to the Mountains"):
           window.location.href =
-            "/pages/readText/A2/week_trip_in_mountains.html";
+            "https://andriilyvyn.github.io/language-page/pages/readText/A2/week_trip_in_mountains.html";
           break;
         case (element = "Going Abroad"):
-          window.location.href = "/pages/readText/B1/going_abroad.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/B1/going_abroad.html";
           break;
         case (element = "Job Searching"):
-          window.location.href = "/pages/readText/B1/job_searching.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/B1/job_searching.html";
           break;
         case (element = "Visiting the Doctor"):
-          window.location.href = "/pages/readText/B1/visiting_the_doctor.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/B1/visiting_the_doctor.html";
           break;
         case (element = "A Walk in the Forest"):
-          window.location.href = "/pages/readText/B1/walk_in_the_forest.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/B1/walk_in_the_forest.html";
           break;
         case (element = "A Typical Day at the Office"):
           window.location.href =
-            "/pages/readText/B2/a_typical_day_at_the_office.html";
+            "https://andriilyvyn.github.io/language-page/pages/readText/B2/a_typical_day_at_the_office.html";
           break;
         case (element = "A Comprehensive Guide to Buying a Car"):
           window.location.href =
-            "/pages/readText/B2/comprehensive_guide_to_buying_a_car.html";
+            "https://andriilyvyn.github.io/language-page/pages/readText/B2/comprehensive_guide_to_buying_a_car.html";
           break;
         case (element = "How to Prepare for a Doctor's Appointment"):
-          window.location.href = "/pages/readText/B2/doctors_Appointment.html";
+          window.location.href = "https://andriilyvyn.github.io/language-page/pages/readText/B2/doctors_Appointment.html";
           break;
         case (element = "Effective Strategies for Client Meetings"):
           window.location.href =
-            "/pages/readText/B2/how_to_conduct_a_successful_client_meeting.html";
+            "https://andriilyvyn.github.io/language-page/pages/readText/B2/how_to_conduct_a_successful_client_meeting.html";
           break;
         case (element = "Mastering Office Culture and Dynamics"):
           window.location.href =
-            "/pages/readText/C1/mastering_office_culture_and_dynamics.html";
+            "https://andriilyvyn.github.io/language-page/pages/readText/C1/mastering_office_culture_and_dynamics.html";
           break;
         case (element = "How to Conduct a Successful Client Meeting"):
           window.location.href =
-            "/pages/readText/C1/effective_strategies_for_client_meetings.html";
+            "https://andriilyvyn.github.io/language-page/pages/readText/C1/effective_strategies_for_client_meetings.html";
           break;
         case (element = "Preparing for a Comprehensive Medical Examination"):
           window.location.href =
-            "/pages/readText/C1/preparing_for_a_comprehensive_medical_examination.html";
+            "https://andriilyvyn.github.io/language-page/pages/readText/C1/preparing_for_a_comprehensive_medical_examination.html";
           break;
         case (element = "The Process of Purchasing a New Vehicle"):
           window.location.href =
-            "/pages/readText/C1/the_process_of_purchasing_a_new_vehicle.html";
+            "https://andriilyvyn.github.io/language-page/pages/readText/C1/the_process_of_purchasing_a_new_vehicle.html";
           break;
       }
     });
